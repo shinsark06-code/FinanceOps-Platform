623fa20fac815aa997b5bd43eee9bf358e2a3226
shinsark06-code <shinsark06@gmail.com>
2026-05-12 23:34:24 +0530

    Add Spring Boot main application class

---
+++ apps/backend/src/main/java/com/financeops/FinanceOpsApplication.java
@@ -0,0 +1,12 @@
+package com.financeops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceOpsApplication.class, args);
+    }
+}
